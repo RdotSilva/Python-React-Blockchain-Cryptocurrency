94b91fa0e02b6f3b2fb747c009be38af68800251
RdotSilva <RyandotSilva@gmail.com>
2020-04-27 20:20:06 -0400

    Update Block attributes

--- block.py
+++ block.py
@@ -4,7 +4,10 @@ class Block:
     Store transactions in a blockchain that supports a cryptocurrency
     """
 
-    def __init__(self, data):
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
 
     def __repr__(self):
