eea5a9da55616a8552b10710884a6756ad60a550
RdotSilva <RyandotSilva@gmail.com>
2020-04-29 21:04:51 -0400

    Add main method

--- crypto_hash.py
+++ crypto_hash.py
@@ -6,3 +6,11 @@ def crypto_hash(data):
     Return a sha-256 hash of the given data.
     """
     return hashlib.sha256(data)
+
+
+def main():
+    print(f"crypto_hash('foo'): {crypto_hash('foo')}")
+
+
+if __name__ == "__main__":
+    main()
