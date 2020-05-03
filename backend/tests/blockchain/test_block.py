0b333f8b54b4259da0878a9f22e6ec06170a98df
RdotSilva <RyandotSilva@gmail.com>
2020-05-02 21:20:07 -0400

    Update test_genesis to compare fields

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,4 @@
-from backend.blockchain.block import Block
+from backend.blockchain.block import Block, GENESIS_DATA
 
 
 def test_mine_block():
@@ -17,4 +17,8 @@ def test_genesis():
     genesis = Block.genesis()
 
     assert isinstance(genesis, Block)
-
+    # Check all fields in genesis against the const fields
+    assert genesis.timestamp == GENESIS_DATA["timestamp"]
+    assert genesis.last_hash == GENESIS_DATA["last_hash"]
+    assert genesis.hash == GENESIS_DATA["hash"]
+    assert genesis.data == GENESIS_DATA["data"]
