5ee953d3ca45a62fb0a3953bf8eb87bdd133ccac
RdotSilva <RyandotSilva@gmail.com>
2020-05-02 21:29:14 -0400

    Add test_add_block method

--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -7,3 +7,12 @@ def test_blockchain_instance():
 
     # Check hash
     assert blockchain.chain[0] == GENESIS_DATA["hash"]
+
+
+def test_add_block():
+    blockchain = Blockchain()
+    data = "test-data"
+    blockchain.add_block(data)
+
+    # Check last block data is equal to test data
+    assert blockchain.chain[-1].data == data
