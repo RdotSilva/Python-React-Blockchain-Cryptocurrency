c90cf8f6ece2835d114fd0da776f37f43d6ee2b6
RdotSilva <RyandotSilva@gmail.com>
2020-05-05 19:36:49 -0400

    Add logic to mine blocks in range

--- backend/scripts/average_block_rate.py
+++ backend/scripts/average_block_rate.py
@@ -6,3 +6,13 @@ from backend.config import SECONDS
 blockchain = Blockchain()
 
 times = []
+
+for i in range(1000):
+    start_time = time.time_ns()
+    blockchain.add_block(i)
+    end_time = time.time_ns()
+
+    time_to_mine = (end_time - start_time) / SECONDS
+    times.append(time_to_mine)
+
+    average_time = sum(times) / len(times)
