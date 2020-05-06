37d5020fbcbb2867027a6dcd33f084869f85511c
RdotSilva <RyandotSilva@gmail.com>
2020-05-06 18:48:16 -0400

    Add hex to binary to increase difficulty precision

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,7 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.util.hex_to_binary import hex_to_binary
 from backend.config import MINE_RATE
 
 GENESIS_DATA = {
@@ -51,7 +52,8 @@ class Block:
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         # Generate a hash with the correct number of leading 0's
-        while hash[0:difficulty] != "0" * difficulty:
+        # Using hex_to_binary to make difficulty check more precise
+        while hex_to_binary(hash)[0:difficulty] != "0" * difficulty:
             nonce += 1
             timestamp = time.time_ns()
             difficulty = Block.adjust_difficulty(last_block, timestamp)
