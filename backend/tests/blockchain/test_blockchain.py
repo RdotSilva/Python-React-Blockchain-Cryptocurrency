f149a19e9f5f59bb568f1b604191bf952f7a3548
RdotSilva <RyandotSilva@gmail.com>
2020-05-09 21:36:45 -0400

    Add test_is_valid_chain method

--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -16,3 +16,14 @@ def test_add_block():
 
     # Check last block data is equal to test data
     assert blockchain.chain[-1].data == data
+
+
+def test_is_valid_chain():
+    # Test will pass if no exceptions raised
+    blockchain = Blockchain()
+
+    # Add 3 blocks to the blockchain instance
+    for i in range(3):
+        blockchain.add_block(i)
+
+    Blockchain.is_valid_chain(blockchain.chain)
