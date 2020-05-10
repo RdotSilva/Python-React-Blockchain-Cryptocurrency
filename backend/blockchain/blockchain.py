923e1dedcc363942ba8df1725cd15c287d107644
RdotSilva <RyandotSilva@gmail.com>
2020-05-10 19:09:27 -0400

    Add replace_chain method

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -18,6 +18,23 @@ class Blockchain:
     def __repr__(self):
         return f"Blockchain: {self.chain}"
 
+    def replace_chain(self, chain):
+        """
+        Replace the local chain with the incoming one if the following applies:
+        1. The incoming chain must be longer than the local one.
+        2. The incoming chain is formatted properly.
+        """
+        if len(chain) <= len(self.chain):
+            raise Exception("Cannot replace. The incoming chain must be longer.")
+
+        try:
+            Blockchain.is_valid_chain(chain)
+        except Exception as e:
+            raise Exception(f"Cannot replace. The incoming chain is invalid: {e}")
+
+        # If the above checks pass, new chain to the old chain spot
+        self.chain = chain
+
     @staticmethod
     def is_valid_chain(chain):
         """
