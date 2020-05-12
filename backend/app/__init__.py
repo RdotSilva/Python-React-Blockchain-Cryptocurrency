8b641b8ac677865869ecbf16bb22747f59b4dd50
RdotSilva <RyandotSilva@gmail.com>
2020-05-11 20:32:38 -0400

    Add blockchain route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 
+from backend.blockchain.blockchain import Blockchain
+
 app = Flask(__name__)
+blockchain = Blockchain()
 
 
 @app.route("/")
-def default():
+def route_default():
     return "Welcome to the blockchain"
+
+
+@app.route("/blockchain")
+def route_blockchain():
+    return blockchain.__repr__()
 
 
 app.run()
