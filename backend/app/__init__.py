2c758916b054392f2990cf2da611d5715ea5a14f
RdotSilva <RyandotSilva@gmail.com>
2020-05-11 20:35:47 -0400

    Update blockchain route to use jsonify

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from backend.blockchain.blockchain import Blockchain
 
 app = Flask(__name__)
 blockchain = Blockchain()
+
+for i in range(3):
+    blockchain.add_block(i)
 
 
 @app.route("/")
@@ -13,7 +16,7 @@ def route_default():
 
 @app.route("/blockchain")
 def route_blockchain():
-    return blockchain.__repr__()
+    return jsonify(blockchain)
 
 
 app.run()
