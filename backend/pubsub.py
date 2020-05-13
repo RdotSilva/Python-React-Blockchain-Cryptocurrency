96651b166ca88d5abab560db2030411105e0aced
RdotSilva <RyandotSilva@gmail.com>
2020-05-12 20:46:23 -0400

    Add PubNub config & setup

---
+++ backend/pubsub.py
@@ -0,0 +1,11 @@
+from pubnub.pubnub import PubNub
+from pubnub.pnconfiguration import PNConfiguration
+
+# Imports from config file, you need to create this and add your pubnub details
+from config import PUBLISH_KEY, SUBSCRIBE_KEY
+
+# Set up pnconfig and pubnub
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = SUBSCRIBE_KEY
+pconfig.publish_key = PUBLISH_KEY
+pubnub = PubNub(pnconfig)
