79cc2d1efbdaf53a531b004bef9c2ccf58cde87e
RdotSilva <RyandotSilva@gmail.com>
2020-05-18 21:10:25 -0400

    Add cryptography logic

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,5 +1,7 @@
 import uuid
 from backend.config import STARTING_BALANCE
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
 
 
 class Wallet:
@@ -11,3 +13,5 @@ class Wallet:
 
     def __init__(self):
         self.address = str(uuid.uuid4())[0:8]
+        self.private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
+        self.public_key = self.private_key.public_key()
