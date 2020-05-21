e88511de12971bafe68df5243739354b5e772440
RdotSilva <RyandotSilva@gmail.com>
2020-05-20 21:13:33 -0400

    Add constructor method

--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -1,4 +1,11 @@
+import uuid
+
+
 class Transaction:
     """
     Document an exchange in currency from a sender to one of more recipients.
     """
+
+    def __init__(self, sender_wallet, recipient, amount):
+        self.id = str(uuid.uuid4())[0:8]
+        self.output = self.create_output(sender_wallet, recipient, amount)
