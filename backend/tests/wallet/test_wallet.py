cd9428cf3fdf96ab81111b81c96b97ece6adc899
RdotSilva <RyandotSilva@gmail.com>
2020-05-20 21:06:54 -0400

    Add test_verify_valid_signature method

--- backend/tests/wallet/test_wallet.py
+++ backend/tests/wallet/test_wallet.py
@@ -1,3 +1,9 @@
 from backend.wallet.wallet import Wallet
 
-# TODO: Add unit tests for Wallet class
+
+def test_verify_valid_signature():
+    data = {"foo": "test_data"}
+    wallet = Wallet()
+    signature = wallet.sign(data)
+
+    assert Wallet.verify(wallet.public_key, data, signature)
