0adcae48825b2814d4f4d7258c1aa2492c58c5b3
RdotSilva <RyandotSilva@gmail.com>
2020-05-21 21:29:14 -0400

    Add input attribute

--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -9,6 +9,7 @@ class Transaction:
     def __init__(self, sender_wallet, recipient, amount):
         self.id = str(uuid.uuid4())[0:8]
         self.output = self.create_output(sender_wallet, recipient, amount)
+        self.input = self.create_input(sender_wallet, self.output)
 
     def create_output(self, sender_wallet, recipient, amount):
         """
