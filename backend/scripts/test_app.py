386dee2dab3def38f93abf7e33ef35aa3f92d994
RdotSilva <RyandotSilva@gmail.com>
2020-05-29 20:35:52 -0400

    Add get_blockchain_mine method

--- backend/scripts/test_app.py
+++ backend/scripts/test_app.py
@@ -1,8 +1,14 @@
 import requests
 
+base_url = "http://localhost:5000"
+
 
 def get_blockchain():
-    return requests.get("http://localhost:5000/blockchain").json()
+    return requests.get(f"{base_url}/blockchain").json()
+
+
+def get_blockchain_mine():
+    return requests.get(f"{base_url}/blockchain/mine").json()
 
 
 start_blockchain = get_blockchain()
