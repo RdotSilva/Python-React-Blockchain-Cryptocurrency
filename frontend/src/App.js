18f8d340198b78c8cd2a1381ccdd3223f1237225
RdotSilva <RyandotSilva@gmail.com>
2020-06-07 20:17:06 -0400

    Add updateUserQuery method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 
 function App() {
   const [userQuery, setUserQuery] = useState("");
+
+  const updateUserQuery = (event) => {
+    console.log("userQuery:", userQuery);
+    setUserQuery(event.target.value);
+  };
+
   return (
     <div className="App">
       <input value={userQuery} onChange={updateUserQuery} />
