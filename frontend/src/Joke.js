9d8539d8614b2e88ad4d2121470c4096b6dbed65
RdotSilva <RyandotSilva@gmail.com>
2020-06-07 20:31:14 -0400

    Add Joke component

---
+++ frontend/src/Joke.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+
+function Joke() {
+  useEffect(() => {
+    fetch("https://official-joke-api.appspot.com/jokes/random")
+      .then((response) => response.json())
+      .then((json) => console.log("Joke JSON:", json));
+    console.log("Fetching data...");
+  });
+
+  return (
+    <div>
+      <h3>Joke</h3>
+    </div>
+  );
+}
+
+export default Joke;
