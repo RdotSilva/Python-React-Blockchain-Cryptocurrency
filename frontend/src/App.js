396a8bed06ef2848ec2fd8a207774b8914fbcc25
RdotSilva <RyandotSilva@gmail.com>
2020-06-09 20:52:03 -0400

    Add walletInfo state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./assets/logo.png";
+
 function App() {
+  const [walletInfo, setWalletInfo] = useState({});
+
   return (
     <div className="App">
       <img className="logo" src={logo} alt="logo" />
