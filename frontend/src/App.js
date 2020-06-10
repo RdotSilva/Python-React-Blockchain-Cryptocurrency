9bc9e0124848187d7bd76e9c266853bcd18e687f
RdotSilva <RyandotSilva@gmail.com>
2020-06-09 20:53:11 -0400

    Add useEffect method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,12 @@ import logo from "./assets/logo.png";
 function App() {
   const [walletInfo, setWalletInfo] = useState({});
 
+  useEffect(() => {
+    fetch("http://localhost:5000/wallet/info")
+      .then((response) => response.json())
+      .then((json) => setWalletInfo(json));
+  }, []);
+
   return (
     <div className="App">
       <img className="logo" src={logo} alt="logo" />
