7af55a2de8a36e49eada2f295cc1b04182b355b3
RdotSilva <RyandotSilva@gmail.com>
2020-06-11 21:17:17 -0400

    Set up block data

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { MILLISECONDS_PY } from "../config";
 
-function Block({ block }) {}
+function Block({ block }) {
+  const { timestamp, hash, data } = block;
+  const hashDisplay = `${hash.substring(0, 15)}...`;
+  const timestampDisplay = new Date(
+    timestamp / MILLISECONDS_PY
+  ).toLocaleString();
+}
 
 export default Block;
