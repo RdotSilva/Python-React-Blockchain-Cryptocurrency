b6761ad381ab07095758eae0d8e31b2960d271a5
RdotSilva <RyandotSilva@gmail.com>
2020-06-12 21:16:05 -0400

    Add transaction info display

--- frontend/src/components/Transaction.js
+++ frontend/src/components/Transaction.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-function Transaction() {}
+function Transaction({ transaction }) {
+  const { input, output } = transaction;
+  const recipients = Objects.keys(output);
+
+  return (
+    <div className="Transaction">
+      <div>From: {input.address}</div>
+      {recipients.map((recipient) => (
+        <div key={recipient}>
+          To: {recipient} | Sent: {output[recipient]}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Transaction;
