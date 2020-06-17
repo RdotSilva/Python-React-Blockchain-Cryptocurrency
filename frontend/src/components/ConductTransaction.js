c9ff74c35abb7ca26198be045a60e5d62805b1d9
RdotSilva <RyandotSilva@gmail.com>
2020-06-16 20:59:46 -0400

    Set up state

--- frontend/src/components/ConductTransaction.js
+++ frontend/src/components/ConductTransaction.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { FormGroup, FormControl, Button } from "react-bootstrap";
 import { API_BASE_URL } from "../config";
 
-function ConductTransaction() {}
+function ConductTransaction() {
+  const [amount, setAmount] = useState(0);
+  const [recipient, setRecipient] = useState("");
+}
 
 export default ConductTransaction;
