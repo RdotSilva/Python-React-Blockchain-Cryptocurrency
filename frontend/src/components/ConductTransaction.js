33c1befcc86926d407393b332f63537d947a30da
RdotSilva <RyandotSilva@gmail.com>
2020-06-16 20:59:09 -0400

    Add ConductTransaction component

---
+++ frontend/src/components/ConductTransaction.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { FormGroup, FormControl, Button } from "react-bootstrap";
+import { API_BASE_URL } from "../config";
+
+function ConductTransaction() {}
+
+export default ConductTransaction;
