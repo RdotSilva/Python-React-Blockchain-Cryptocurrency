28c81c100a91d2bcac6055336a0e75532b8a0c2c
RdotSilva <RyandotSilva@gmail.com>
2020-06-17 20:09:00 -0400

    Add Route for main App

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import "./index.css";
 import App from "./App";
+import Blockchain from "./components/Blockchain";
+import ConductTransaction from "./components/ConductTransaction";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
