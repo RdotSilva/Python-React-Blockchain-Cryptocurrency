091e04638c7866c7038c28a5f7cc13330f7538aa
RdotSilva <RyandotSilva@gmail.com>
2020-06-20 21:04:24 -0400

    Add TransactionPool component

---
+++ frontend/src/components/TransactionPool.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Transaction from "./Transaction";
+import { API_BASE_URL } from "../config";
+
+function TransactionPool() {}
+
+export default TransactionPool;
