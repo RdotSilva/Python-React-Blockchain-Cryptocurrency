b514e5b4c5ab9e7de2819851de8bb832bce6e15a
RdotSilva <RyandotSilva@gmail.com>
2020-06-20 21:05:27 -0400

    Add useEffect method

--- frontend/src/components/TransactionPool.js
+++ frontend/src/components/TransactionPool.js
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 import Transaction from "./Transaction";
 import { API_BASE_URL } from "../config";
 
-function TransactionPool() {}
+function TransactionPool() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/transactions`)
+      .then((response) => response.json())
+      .then((json) => setTransactions(json));
+  }, []);
+}
 
 export default TransactionPool;
