bb65e2aa0b0f65fbdb7e60435cf04dbdcc7233a5
RdotSilva <RyandotSilva@gmail.com>
2020-06-23 21:26:50 -0400

    Add home link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <Nav fill variant="tabs" defaultActiveKey="/">
+      <Nav.Item>
+        <Nav.Link href="/">Home</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
 }
 
 export default Navbar;
