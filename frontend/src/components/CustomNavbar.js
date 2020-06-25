9db27b1ee8d9f9fcd073bced8a0721a982374719
RdotSilva <RyandotSilva@gmail.com>
2020-06-24 20:47:35 -0400

    Add home link to new navbar

--- frontend/src/components/CustomNavbar.js
+++ frontend/src/components/CustomNavbar.js
@@ -3,7 +3,13 @@ import { Navbar, Nav } from "react-bootstrap";
 
 function CustomNavbar() {
   return (
-    
+    <Navbar bg="light" expand="lg">
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
